0e5284e4796ef8200aeffc7055d7defc8970ff1c
profe-givox <gi.vega@itsur.edu.mx>
2020-09-17 18:32:58 -0500

    Se versiono por primera vez el proyecto. Se códifico los metdos del ciclo de vida de una actividad

---
+++ app/src/main/java/net/ivanvega/miprimerproyectoandroid/ActivityDos.java
@@ -0,0 +1,54 @@
+package net.ivanvega.miprimerproyectoandroid;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityDos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_segunda_activity);
+
+        Log.i("acti", "paso por oncreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("acti", "paso por onstart") ;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("acti", "paso por onResume") ;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("acti", "Paso poe stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("acti", "Paso poe stop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("acti", "Paso poe pause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("acti", "Paso poe destroy");
+    }
+}
---
+++ app/src/main/java/net/ivanvega/miprimerproyectoandroid/MainActivity.java
@@ -0,0 +1,19 @@
+package net.ivanvega.miprimerproyectoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
